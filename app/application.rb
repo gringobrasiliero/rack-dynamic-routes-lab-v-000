4074ff8cd40941bbaa22814e85cf3821496bda63
Nolan Byrnes <nolanehs@gmail.com>
2018-01-13 01:01:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ def call(env)
 resp = Rack::Response.new
 req = Rack::Request.new(env)
 
-  if req.path=="/items/"
+  if req.path.match=="/items/"
     item_name = req.path.split("/items/").last
     item = Item.all.find{|i| i.name == item_name}
     if item != nil
